e459d6ee6d257d33f2e3d8a9c9f10c4f96dd816d
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-04 14:32:21 +0530

    uc-3

---
+++ EmpPartTime/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EmployeePartTimeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int isFullTime = 1;
+            int isPartTime = 2;
+            int empWage = 0;
+            int empHours = 0;
+            int empRatePerHours = 20;
+            string empstatus = "";
+            Random rand = new Random();
+            int empCheck = rand.Next(0, 3);
+            if (empCheck == isFullTime)
+            {
+                empHours = 8;
+                empstatus = "FullTime";
+            }
+            else if (empCheck == isPartTime)
+            {
+                empHours = 4;
+                empstatus = "PartTime";
+            }
+            else
+            {
+                empHours = 0;
+                empstatus = "Absent";
+            }
+
+            empWage = empRatePerHours * empHours;
+            Console.WriteLine("Employee wage is " + empWage + " Employee is " + empstatus);
+            Console.ReadKey();
+        }
+    }
+}
