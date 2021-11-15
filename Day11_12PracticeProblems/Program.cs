4befd53b6cacb6e77f4509bb569159e5ad21366a
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-15 15:49:42 +0530

    Day11&12 Inventory Program pushed

---
+++ Day11_12PracticeProblems/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Day11_12PracticeProblems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string Path = "E:/vs/inventory.json";
+            Console.WriteLine("Welcome to the Inventory Data Management.!!!");
+
+            ReadInventoryData readInventoryData = new ReadInventoryData();
+            InventoryManagement data = readInventoryData.Read(Path);
+
+            for (int i = 0; i < data.Rice.Count; i++)
+            {
+                Console.WriteLine("Rice Details");
+                int valueofRice = data.Rice[i].weight * data.Rice[i].price;
+                Console.WriteLine("The value of {0} is {1}", data.Rice[i].name, valueofRice);
+                Console.WriteLine("---------------------------------------------------------");
+            }
+
+
+            for (int i = 0; i < data.Pulses.Count; i++)
+            {
+                Console.WriteLine("Pulses Details");
+                int valueofPulses = data.Pulses[i].weight * data.Pulses[i].price;
+                Console.WriteLine("The value of {0} is {1}", data.Pulses[i].name, valueofPulses);
+                Console.WriteLine("---------------------------------------------------------");
+            }
+
+
+            for (int i = 0; i < data.Wheat.Count; i++)
+            {
+                Console.WriteLine("Wheat Details");
+                int valueofWheat = data.Wheat[i].weight * data.Wheat[i].price;
+                Console.WriteLine("The value of {0} is {1}", data.Wheat[i].name, valueofWheat);
+                Console.WriteLine("---------------------------------------------------------");
+            }
+        }
+    }
+}
+
