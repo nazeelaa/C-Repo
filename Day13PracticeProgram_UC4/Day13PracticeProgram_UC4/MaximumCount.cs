8cae353dddccf59fa5397988def292ecdc9c6527
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-17 19:01:47 +0530

    Day 13 Practice Programs uc4 pushed

---
+++ Day13PracticeProgram_UC4/Day13PracticeProgram_UC4/MaximumCount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13PracticeProgram_UC4
+{
+
+	public class MaximumCount<T> where T : IComparable<T>
+	{
+		List<T> maxValue = new List<T>();
+		public MaximumCount(params T[] values)
+		{
+			this.ValuesBuilder(values);
+		}
+		public void ValuesBuilder(T[] maxValue)
+		{
+			foreach (T item in maxValue)
+			{
+				this.maxValue.Add(item);
+			}
+		}
+		public void Values()
+		{
+			this.maxValue.Sort();
+			Console.WriteLine(this.maxValue[this.maxValue.Count - 1]);
+		}
+	}
+}
+
