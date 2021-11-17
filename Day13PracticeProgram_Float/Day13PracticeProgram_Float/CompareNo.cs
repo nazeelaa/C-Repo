7a69a820caf5bd898ec7a5cf661ff5c6031aceb5
nazeelaa <92171051+nazeelaa@users.noreply.github.com>
2021-11-17 18:58:55 +0530

    Day 13 UC2 Pushed

---
+++ Day13PracticeProgram_Float/Day13PracticeProgram_Float/CompareNo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13PracticeProgram_Float
+{
+    class CompareNo
+    {
+
+        public static void compareNo()
+        {
+            double x = 34.3, y = 45.8, z = 42.5;
+
+            if (x.CompareTo(y) > 0 && x.CompareTo(z) > 0 || x.CompareTo(y) >= 0 && x.CompareTo(z) > 0 || x.CompareTo(y) > 0 && x.CompareTo(z) > 0)
+            {
+                Console.WriteLine(x + " Greater than " + y + " and " + z);
+            }
+            else if (y.CompareTo(x) > 0 && y.CompareTo(z) > 0 || y.CompareTo(x) >= 0 && y.CompareTo(z) > 0 || y.CompareTo(x) > 0 && y.CompareTo(z) > 0)
+            {
+                Console.WriteLine(y + " Greater than " + x + " and " + z);
+            }
+            else if (z.CompareTo(x) > 0 && z.CompareTo(y) > 0 || z.CompareTo(x) >= 0 && z.CompareTo(y) > 0 || z.CompareTo(x) > 0 && z.CompareTo(y) > 0)
+            {
+                Console.WriteLine(z + " Greater than " + x + " and " + y);
+            }
+        }
+    }
+}
+    
